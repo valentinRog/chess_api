6f70133ea815d42d111027c550a47a45c57e9307
Valentin Rogister <valentin.rogister@gmail.com>
2022-07-31 21:48:33 +0200

    yo

---
+++ build_database.py
@@ -0,0 +1,20 @@
+import pymongo
+import csv
+import chess
+
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client["puzzles"]
+collection = db["puzzles"]
+
+csvfile = open('puzzles.csv', 'r')
+reader = csv.reader( csvfile )
+
+with open('puzzles.csv', 'r') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        row["Rating"] = int(row["Rating"])
+        row["RatingDeviation"] = int(row["RatingDeviation"])
+        row["Popularity"] = int(row["Popularity"])
+        row["NbPlays"] = int(row["NbPlays"])
+        row["NbPieces"] = len(chess.Board(row["FEN"]).piece_map())
+        collection.insert_one(row)
