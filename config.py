cf460f299b7808d8a01e515aa5c7395edf7d3157
valentinRog <97336344+valentinRog@users.noreply.github.com>
2022-10-11 14:08:15 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,13 +5,6 @@ SQLALCHEMY_DATABASE_URI = "mysql://{user}:{password}@{address}:{port}/{database}
     port="3306",
     database="puzzles"
 )
-# SQLALCHEMY_DATABASE_URI = "mysql://{user}:{password}@{address}:{port}/{database}".format(
-#     user="valenbel123",
-#     password="mysqlpassword",
-#     address="valenbel123.mysql.pythonanywhere-services.com",
-#     port="3306",
-#     database="valenbel123$puzzles"
-# )
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_POOL_RECYCLE = 299
 SQLALCHEMY_POOL_TIMEOUT = 20
